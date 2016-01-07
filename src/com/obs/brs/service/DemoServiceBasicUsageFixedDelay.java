72dbe5c44b4186ed030affc1237ad42c49f0156d
Ahemad <ahemad.tutake@gmail.com>
2016-01-07 19:28:09 +0530

    test for method call

--- src/com/obs/brs/service/DemoServiceBasicUsageFixedDelay.java
+++ src/com/obs/brs/service/DemoServiceBasicUsageFixedDelay.java
@@ -27,7 +27,7 @@ public class DemoServiceBasicUsageFixedDelay
 	@ManagedProperty(value ="#{DeService}")
 	IDeService deService;
     
-	@Scheduled(fixedRate = 3600000, initialDelay=10*60*1000)
+	@Scheduled(fixedRate = 3600000, initialDelay= 2 *60*1000)
     //@Scheduled(fixedRate = 5000)
     public void demoServiceMethod(){
     	System.out.println("Method executed at every 1 hour. Current time is :: "+ new Date());
