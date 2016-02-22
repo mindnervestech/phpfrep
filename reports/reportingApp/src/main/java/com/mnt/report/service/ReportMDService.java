3adc2de0da85f13f277c1eb3010482d7536b299b
Ahemad <ahemad.tutake@gmail.com>
2016-02-22 12:03:22 +0530

    image urls

--- reports/reportingApp/src/main/java/com/mnt/report/service/ReportMDService.java
+++ reports/reportingApp/src/main/java/com/mnt/report/service/ReportMDService.java
@@ -70,6 +70,8 @@ public class ReportMDService {
 	
 	@Autowired
 	NamedParameterJdbcTemplate namedJdbcTemplate;
+	@Value("${imagePath}")
+	String imgPath;
 	
 	private static Map<String,String> titleMap = new HashMap<String,String>(6);
 	private static Map<String,String> sectionMap = new HashMap<String,String>(4);
@@ -842,6 +844,28 @@ public class ReportMDService {
 			result = id+"/"+mdResult.get("DC_IMAGENAME").toString();
 		}
 		return result;
+	}
+	
+	@RequestMapping(value="/get-parent-image",method=RequestMethod.GET)
+	@ResponseBody
+	public FileSystemResource getParentImageImg(@RequestParam("id") Long id) {
+		Map<String,Object> mdResult = jt.queryForMap("select DC_IMAGENAME from tbl_parent_image where DN_ID ="+id);
+		String result = "";
+		if(mdResult!=null &&!mdResult.isEmpty()) {
+			result = imgPath+id+File.separator+mdResult.get("DC_IMAGENAME").toString();
+		}
+		return new FileSystemResource(result);
+	}
+	
+	@RequestMapping(value="/get-child-image",method=RequestMethod.GET)
+	@ResponseBody
+	public FileSystemResource getChildImageImg(@RequestParam("id") Long id) {
+		Map<String,Object> mdResult = jt.queryForMap("select DC_IMAGENAME,DN_PARENT_IMAGE_ID from tbl_child_image where DN_ID ="+id);
+		String result = "";
+		if(mdResult!=null &&!mdResult.isEmpty()) {
+			result = imgPath+mdResult.get("DN_PARENT_IMAGE_ID").toString()+File.separator+id+File.separator+mdResult.get("DC_IMAGENAME").toString();
+		}
+		return new FileSystemResource(result);
 	}
 	
 	@RequestMapping(value="/getChildImage",method=RequestMethod.GET)
